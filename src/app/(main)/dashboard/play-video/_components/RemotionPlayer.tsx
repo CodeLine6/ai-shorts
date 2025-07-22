f6063111d21c53cbfa434227cafbd14626f1fd45
CodeLine6 <chaudharymaneet93@gmail.com>
2025-07-22 21:21:04 +0530

    Refactor: Improve Remotion player duration calculation with typed captions
    Introduce `captions` variable typed as `sentence[]` for `videoData.captionJson`.
    Use the typed `captions` variable to calculate `durationInFrames` for the Remotion player, enhancing type safety and readability.

--- src/app/(main)/dashboard/play-video/_components/RemotionPlayer.tsx
+++ src/app/(main)/dashboard/play-video/_components/RemotionPlayer.tsx
@@ -2,13 +2,14 @@
 import RemotionComposition from "@/app/_components/RemotionComposition";
 import { Player } from "@remotion/player";
 import { useState } from "react";
+import { sentence } from "../../../../../../convex/schema";
 
 const RemotionPlayer = ({ videoData }: { videoData: any }) => {
-
+const captions: sentence[] = videoData?.captionJson
   return (
     <Player
       component={RemotionComposition}
-      durationInFrames={parseInt((videoData.captionJson[videoData.captionJson.length - 1].end).toFixed(0)) * 30}
+      durationInFrames={parseInt(captions[captions.length - 1].end.toFixed(0)) * 30}
       fps={30}
       compositionWidth={720}
       compositionHeight={1280}
